9b4bd0cd33d7907ea6900e1ab385b048e2f81cf7
malidevf <mali@devf.mx>
2020-08-18 23:41:00 -0500

    cambios

---
+++ Boostrap/script.js
@@ -0,0 +1,7 @@
+function sayHello () {
+    const hello = 'Hello CSS-Tricks Reader!'
+    console.log(hello)
+  }
+  
+  sayHello() // 'Hello World'
+  console.log(hello) // Error, hello is not defined
